db9c3e32714c1a1d38404cae13cf501f2c1c459d
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2022-02-14 16:40:02 +0100

    added todo in MainActivity.java

--- app/src/main/java/com/portofos/navigableme/MainActivity.java
+++ app/src/main/java/com/portofos/navigableme/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // TODO: Now when we have all five fragments ready we need to initialize BottomNavigationView
+    // TODO: and all five fragments in MainActivity.java
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
