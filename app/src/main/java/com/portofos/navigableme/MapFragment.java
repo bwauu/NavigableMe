9413889ac75dec95580408d3ca3f5bd026868ce6
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2022-02-15 17:48:51 +0100

    Added to do in MapFragment.java

--- app/src/main/java/com/portofos/navigableme/MapFragment.java
+++ app/src/main/java/com/portofos/navigableme/MapFragment.java
@@ -16,7 +16,7 @@ public class MapFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map, container, false);
 
-        // TODO: Implement google maps sdk api
+        // TODO: Implement desired functionality like markers and so
 
     }
 }
