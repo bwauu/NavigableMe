236ccd0ffb0aa9380a10efb468706adb4ad7f9e1
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2022-02-15 17:04:41 +0100

    Added play-services-maps library see build.gradle Instructions in MapFragment.java for Android Studio Bumblebee

--- app/src/main/java/com/portofos/navigableme/MapFragment.java
+++ app/src/main/java/com/portofos/navigableme/MapFragment.java
@@ -17,5 +17,14 @@ public class MapFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_map, container, false);
 
         // TODO: Implement google maps sdk api
+        // 1. Click on "app" folder and press F4 || Right-click>app>Open Module Settings
+        // 2. In settings go>Dependencies
+        // 3. In Dependencies, click on '+' || (windows) alt+insert
+        // 4. Click on  Library Dependency
+        // 5. Search for "play-services-maps"
+        // 6. Select artefact play-services-maps and click ok
+        // 7. Click on 'apply'
+        // 8. Click then on Ok.
+        // Added Library Completed
     }
 }
