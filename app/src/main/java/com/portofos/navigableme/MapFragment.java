f3994770e8c7c0c68863feb2ccf52c622b2a0d52
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2022-02-21 13:03:18 +0100

    Working version. onClick display pinpoint on map, and if you click on that pinpoint you will be displayed information about the location. Query search is implemented and working in this version

--- app/src/main/java/com/portofos/navigableme/MapFragment.java
+++ app/src/main/java/com/portofos/navigableme/MapFragment.java
@@ -1,30 +1,42 @@
 package com.portofos.navigableme;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapFragment extends Fragment {
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     // TODO: Implement onQueryListner to search for stockholm for example and get pinpoint to stockholm
     // IMPLEMENTATION:
+    GoogleMap googleMap;
+    private MapView mapView;
+    SearchView searchView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // https://www.geeksforgeeks.org/how-to-add-searchview-in-google-maps-in-android/ implemention test before savepoint
+
         // Initialize view
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
@@ -32,7 +44,10 @@ public class MapFragment extends Fragment {
         SupportMapFragment supportMapFragment = (SupportMapFragment)
                 getChildFragmentManager().findFragmentById(R.id.google_map);
 
+        searchView = view.findViewById(R.id.searchView_mapfragment);
+
         // Async map
+        /*
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
@@ -58,9 +73,74 @@ public class MapFragment extends Fragment {
                     }
                 });
             }
-        });
+        }); */
         // Return view
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) view.findViewById(R.id.google_map);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                // When clicked on map
+                // Intitialize marker options
+                MarkerOptions markerOptions = new MarkerOptions();
+                // Set position of marker
+                markerOptions.position(latLng);
+                // Set title of marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                // Remove all marker
+                googleMap.clear();
+                // Animating to zoom the marker
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        latLng, 10
+                ));
+                // Add marker on map
+                googleMap.addMarker(markerOptions);
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if(location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(getActivity());
+                    try {
+                        addressList = geocoder.getFromLocationName(location,1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    googleMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,20));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        mapView.getMapAsync(this);
 
     }
 }
